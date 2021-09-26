3543c916d14a3f108ae7a250b38570f86228b100
Eduardo Patrocinio <epatro@gmail.com>
2021-09-26 19:48:43 -0400

    Build IBM MQ images

---
+++ build_docker_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+docker build --tag="patrocinio/jmeter-base:latest" -f Dockerfile-base .
+docker build --tag="patrocinio/jmeter-master:latest" -f Dockerfile-master .
+docker build --tag="patrocinio/jmeter-slave:latest" -f Dockerfile-slave .
+docker build --tag="patrocinio/jmeter-reporter" -f Dockerfile-reporter .
+docker build --tag="patrocinio/jmeter-base-ibmmq:latest" -f Dockerfile-base-ibmmq .
+docker build --tag="patrocinio/jmeter-master-ibmmq:0.1" -f Dockerfile-master-ibmmq .
+docker build --tag="patrocinio/jmeter-slave-ibmmq:0.2" -f Dockerfile-master-ibmmq .
+
+
