fe1fc1fc18c1b87ba5149560cd36260139d814d6
Eduardo Patrocinio <epatro@gmail.com>
2021-09-26 20:04:10 -0400

    Fix IBM MQ slave image

--- push_docker_images.sh
+++ push_docker_images.sh
@@ -1,3 +1,3 @@
 docker push patrocinio/jmeter-master:latest
 docker push patrocinio/jmeter-master-ibmmq:0.1
-docker push patrocinio/jmeter-slave-ibmmq:0.2
+docker push patrocinio/jmeter-slave-ibmmq:0.3
